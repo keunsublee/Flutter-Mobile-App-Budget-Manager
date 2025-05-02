a2b25d8f3092feac641210fffe72b4daf92029ce
Gavin Xiao <gavin.xiao381@gmail.com>
2025-05-02 16:29:08 -0400

    tried to connect to cloud SQL

--- backend/server/bin/server.dart
+++ backend/server/bin/server.dart
@@ -3,12 +3,17 @@ import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:postgres/postgres.dart';
 
 // Configure routes.
 final _router =
     Router()
       ..get('/', _rootHandler)
       ..get('/echo/<message>', _echoHandler);
+
+// Database 
+// ignore: prefer_typing_uninitialized_variables
+late var database; 
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
@@ -19,7 +24,32 @@ Response _echoHandler(Request request) {
   return Response.ok('$message\n');
 }
 
+// TODO: POST Request for putting data into database for all three tables
+
+// TODO: GET  Request for grabbing data from database from all three tables 
+
+// TODO: PUT  Request for updating table in database for all three tables   
+
 void main(List<String> args) async {
+  // TODO: Connect to PostgreSQL Database
+  print('INSTANCE_CONNECTION_NAME: ${Platform.environment['INSTANCE_CONNECTION_NAME']}');
+  print('DB_USER: ${Platform.environment['DB_USER']}');
+  print('DB_NAME: ${Platform.environment['DB_NAME']}');
+  print('DB_PASS: ${Platform.environment['DB_PASS']}');
+  
+
+  database = await Connection.open(
+    Endpoint(
+      host: '/cloudsql/${Platform.environment['INSTANCE_CONNECTION_NAME']}',
+      database: Platform.environment['DB_NAME'] ?? "",
+      username: Platform.environment['DB_USER'],
+      password: Platform.environment['DB_PASS'],
+      port: 5432,
+      isUnixSocket: true,
+      ),
+  );
+  print('✅ Connected to Cloud SQL from Cloud Run!');
+
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
